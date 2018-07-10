6173023c85005ca5b886272fe6433e4202438800
Sean B <sean.bradley10@gmail.com>
2018-07-10 08:55:11 +0000

    add mlab

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+
+
+mongoose.connect("mongodb://sebradle:max1234@ds133041.mlab.com:33041/sebradleyelpcamp");
+//mongoose.connect("mongodb://localhost/yelp_camp_v12");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
